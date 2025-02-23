892a8f2f492d57bc5fbaed8276102a8fbfbf291f
AdelinaMoroaca <moroacaadelina@gmail.com>
2025-02-24 01:28:11 +0200

    Create OSF project

---
+++ cartridges/app_training/cartridge/controllers/Test.js
@@ -0,0 +1,25 @@
+// 'use strict'
+// var server =require('server');
+
+// server.get('HelloWorld', function (req, res, next) {
+//     var viewData = res.getViewData();
+//     viewData.myvariable = 32;
+//     res.setViewData(viewData);
+//     res.render("testtemplate/mytemplate", { viewData: viewData });
+//     return next();
+// });
+// module.exports = server.exports();
+
+
+var server = require('server');
+var assets = require('*/cartridge/scripts/assets.js');
+
+server.get('Show', function (req, res, next) {
+    assets.addJs('/js/productTile.js');
+    assets.addCss('/css/homePage.css');
+    
+    res.render('homePage'); 
+    next();
+});
+
+module.exports = server.exports();
